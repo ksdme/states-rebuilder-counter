c6a528430cc5dc1160d53cc2ac60638bb740e26d
Kilari Teja <kilariteja9@gmail.com>
2019-12-23 00:00:25 +0530

    Use states_rebuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:states_rebuilder/states_rebuilder.dart';
 
 void main() => runApp(MyApp());
+
+class CounterBloc {
+  int counter = 0;
+  static final instance = CounterBloc();
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Injector(
+        inject: [Inject(() => CounterBloc.instance)],
+        builder: (_) => HomePage(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final model = Injector.getAsReactive<CounterBloc>(
+      context: context,
+    );
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(
+          'States Rebuilder Counter',
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              '$_counter',
+              '${model.state.counter}',
               style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => model.setState((state) => state.counter++),
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
